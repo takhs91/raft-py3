3a396b158a4513848934199eda29cede06cee577
Takis Panagopoulos <takhs91@gmail.com>
2020-06-21 03:50:53 +0300

    Implement leader election v1

--- raft.py
+++ raft.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import random
+import rpyc
 import threading
 import time
 
@@ -21,11 +22,19 @@ CANDIDATE = "candidate"
 LEADER = "leader"
 
 
+class Peer:
+    def __init__(self, hostname, port):
+        self.hostname = hostname
+        self.port = port
+        self.conn = None
+
+
 class RaftServer(Service):
     def __init__(self, id, peers):
         # ID, peers
         self.id = id
         self.peers = peers
+        self.leader_id = None
 
         # persistant state
         self.current_term = 0
@@ -55,18 +64,27 @@ class RaftServer(Service):
         self.candidate_event = threading.Event()
         self.candidate_timer_reset_event = threading.Event()
         self.candidate_timer_expired_event = threading.Event()
-        self.election_timer = threading.Thread(
+        self.candidate_election_timer = threading.Thread(
             target=self.candidate_election_timeout,
             args=('candidate_election_timeout',),
             daemon=True,
         )
-        self.election_timer.start()
+        self.candidate_election_timer.start()
         self.candidate_loop = threading.Thread(
             target=self.candidate_loop,
             args=('candidate_loop',),
             daemon=True,
         )
         self.candidate_loop.start()
+        # Leader stuff
+        self.leader_event = threading.Event()
+        self.leader_loop = threading.Thread(
+            target=self.leader_loop,
+            args=('leader_loop',),
+            daemon=True,
+        )
+        self.leader_loop.start()
+
 
     def switch_state_to(self, state):
         with self.lock:
@@ -124,7 +142,6 @@ class RaftServer(Service):
                     break
                 logger.info("Thread %s: Timed out: Starting new election", name)
                 self.candidate_timer_expired_event.set()
-                self.candidate_timer_reset_event.clear()
                 continue
 
             logger.info("Thread %s: Reseting timer", name)
@@ -147,14 +164,113 @@ class RaftServer(Service):
                     name,
                     self.current_term
                 )
-            self.candidate_timer_expired_event.wait()
+            votes = 1
+            # Setup RPC connections
+            for peer_id in self.peers:
+                if not self.peers[peer_id].conn:
+                    hostname = self.peers[peer_id].hostname
+                    port = self.peers[peer_id].port
+                    try:
+                        self.peers[peer_id].conn = rpyc.connect(hostname, port,
+                                                                keepalive=True)
+                    except ConnectionError:
+                        pass
+            async_results = []
+            # Send async RequestVoteRPC
+            for peer_id in self.peers:
+                conn = self.peers[peer_id].conn
+                if conn:
+                    try:
+                        request_vote_async = rpyc.async_(conn.root.request_vote_rpc)
+                        async_results.append(request_vote_async(self.current_term, self.id))
+                    except EOFError:
+                        self.peers[peer_id].conn = None
+
+            while True:
+                if self.candidate_timer_expired_event.is_set():
+                    break
+                returned_results = []
+                for async_result in async_results:
+                    if async_result.ready:
+                        returned_results.append(async_result)
+                        value = async_result.value
+                        term, voted_granted = value
+                        if voted_granted:
+                            votes += 1
+                async_results = [result for result in async_results if result not in returned_results]
+                # If collected majority become leader
+                if votes >= len(self.peers) // 2 + 1:
+                    logger.info("Thread %s: Switching to LEADER", name)
+                    self.switch_state_to(LEADER)
+                    break
+
             self.candidate_timer_expired_event.clear()
 
-    def exposed_append_entries_rpc(self):
+    def leader_loop(self, name):
+        """The leader loop
+        """
+        logger.info("Thread %s: starting", name)
+        while not self.stop_event.is_set():
+            # Must be a leader to run
+            if not self.leader_event.is_set():
+                logger.info("Thread %s: Not LEADER, will wait now..", name)
+            self.leader_event.wait()
+            logger.info("Thread %s: Became LEADER.", name)
+            while True:
+                logger.info("Thread %s: Sending Heartbeats...", name)
+                # Setup RPC connections
+                for peer_id in self.peers:
+                    if not self.peers[peer_id].conn:
+                        hostname = self.peers[peer_id].hostname
+                        port = self.peers[peer_id].port
+                        try:
+                            self.peers[peer_id].conn = rpyc.connect(hostname, port,
+                                                                    keepalive=True)
+                        except ConnectionError:
+                            pass
+                async_results = []
+                # Send async AppendEntriesRPC as heartbeats
+                for peer_id in self.peers:
+                    conn = self.peers[peer_id].conn
+                    if conn:
+                        try:
+                            request_vote_async = rpyc.async_(conn.root.append_entries_rpc)
+                            async_results.append(request_vote_async(self.current_term, self.id))
+                        except EOFError:
+                            self.peers[peer_id].conn = None
+                time.sleep(2)
+
+    def exposed_request_vote_rpc(self, term, candidate_id):
+        """Request Votes RPC
+        """
+        logger.info("Called the Request Votes RPC")
+        logger.info(term)
+        logger.info(self.current_term)
+        with self.lock:
+            if term < self.current_term:
+                return self.current_term, False
+            else:
+                self.voted_for = candidate_id
+        if term > self.current_term:
+            with self.lock:
+                self.current_term = term
+            self.switch_state_to(FOLLOWER)
+        self.reset_event.set()
+        return term, True
+
+    def exposed_append_entries_rpc(self, term, leader_id):
         """Append Entries RPC
         """
         logger.info("Called the Append Entries RPC")
+        with self.lock:
+            if term < self.current_term:
+                return self.current_term, False
+            else:
+                self.leader_id = leader_id
+        self.switch_state_to(FOLLOWER)
         self.reset_event.set()
+        self.candidate_timer_reset_event.set()
+        return term, True
 
     def exposed_is_leader(self):
         return False
@@ -170,10 +286,12 @@ def parse_raft_config(raft_config_filename):
             node_number = int(node_name[len('node'):])
             hostname, port = params.strip().split(':', maxsplit=1)
             logger.debug((node_number, hostname, port))
-            nodes[node_number] = {'hostname': hostname, 'port': int(port)}
+            nodes[node_number] = Peer(hostname=hostname, port=int(port))
     return nodes
 
+
 nodes = None
+
 
 if __name__ == '__main__':
     # Setup argument parser
@@ -189,7 +307,8 @@ if __name__ == '__main__':
     if args.node_number not in nodes:
         raise Exception('Node number does not exist in the configuration')
     node = nodes[args.node_number]
-    nodes = nodes.pop(args.node_number)
+    nodes.pop(args.node_number)
 
-    s = ThreadPoolServer(RaftServer(id=args.node_number, peers=nodes), hostname=node['hostname'], port=node['port'])
+    s = ThreadPoolServer(RaftServer(id=args.node_number, peers=nodes),
+                         hostname=node.hostname, port=node.port)
     s.start()
